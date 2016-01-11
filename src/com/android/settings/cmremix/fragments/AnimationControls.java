f75451c326c0532e3140098e7b676536ea467fdf
ZION959 <ziontran@gmail.com>
2016-01-11 11:10:35 -0700

    set animation duration to 5

--- src/com/android/settings/cmremix/fragments/AnimationControls.java
+++ src/com/android/settings/cmremix/fragments/AnimationControls.java
@@ -170,7 +170,7 @@ public class AnimationControls extends SettingsPreferenceFragment implements OnP
         mTaskOpenBehind.setEntryValues(mAnimationsNum);
 
         int defaultDuration = Settings.System.getInt(mContentRes,
-                Settings.System.ANIMATION_CONTROLS_DURATION, 10);
+                Settings.System.ANIMATION_CONTROLS_DURATION, 5);
         mAnimationDuration = (SlimSeekBarPreference) findPreference(ANIMATION_DURATION);
         mAnimationDuration.setInitValue((int) (defaultDuration));
         mAnimationDuration.setOnPreferenceChangeListener(this);
