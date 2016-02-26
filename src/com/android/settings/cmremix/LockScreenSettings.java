33c57fb34753ee74b6655710940ffcc10b496fa1
beanstown106 <nbenis106@gmail.com>
2016-02-25 22:17:32 -0700

    [SQUASHED] Configurable Keygaurd Alpha and Bouncer Bypass [2/2]
    Conflicts:
    	res/values/cmremix_strings.xml
    	res/values/rr_arrays.xml
    	res/xml/rr_ls_security.xml
    	res/xml/rr_ls_ui.xml
    	src/com/android/settings/rr/LockScreenSecurity.java
    	src/com/android/settings/rr/LockscreenUI.java
    
    (cherry picked from commit 807c65bdc6c6dae43a284d040cfef76d167023ff)

--- src/com/android/settings/cmremix/LockScreenSettings.java
+++ src/com/android/settings/cmremix/LockScreenSettings.java
@@ -16,28 +16,41 @@
 
 package com.android.settings.cmremix;
 
+import android.app.Activity;
+import android.content.Context;
 import android.content.ContentResolver;
+import android.content.res.Resources;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.UserHandle;
+import android.os.UserManager;
 import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.Preference.OnPreferenceChangeListener;
 import android.preference.PreferenceFragment;
 import android.preference.PreferenceScreen;
+import android.preference.SwitchPreference;
 import android.provider.Settings;
 
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.cmremix.utils.SeekBarPreference;
 
 import com.android.internal.logging.MetricsLogger;
 
 public class LockScreenSettings extends SettingsPreferenceFragment implements OnPreferenceChangeListener {
 
     private static final String TAG = "LockScreenSettings";
-
     private static final String LOCK_CLOCK_FONTS = "lock_clock_fonts";
+    private static final String PREF_LS_BOUNCER = "lockscreen_bouncer";
+    private static final String LOCKSCREEN_SECURITY_ALPHA = "lockscreen_security_alpha";
+    private static final String LOCKSCREEN_ALPHA = "lockscreen_alpha";	
 
     private ListPreference mLockClockFonts;
+    private ListPreference mLsBouncer;
+    private SeekBarPreference mLsSecurityAlpha;
+    private SeekBarPreference mLsAlpha;
 
     @Override
     protected int getMetricsCategory() {
@@ -56,7 +69,54 @@ public class LockScreenSettings extends SettingsPreferenceFragment implements On
                 resolver, Settings.System.LOCK_CLOCK_FONTS, 4)));
         mLockClockFonts.setSummary(mLockClockFonts.getEntry());
         mLockClockFonts.setOnPreferenceChangeListener(this);
+
+	mLsAlpha = (SeekBarPreference) findPreference(LOCKSCREEN_ALPHA);
+            float alpha = Settings.System.getFloat(resolver,
+                   Settings.System.LOCKSCREEN_ALPHA, 0.45f);
+            mLsAlpha.setValue((int)(100 * alpha));
+            mLsAlpha.setOnPreferenceChangeListener(this);
+
+	 mLsBouncer = (ListPreference) findPreference(PREF_LS_BOUNCER);
+        mLsBouncer.setOnPreferenceChangeListener(this);
+        int lockbouncer = Settings.Secure.getInt(resolver,
+                Settings.Secure.LOCKSCREEN_BOUNCER, 0);
+        mLsBouncer.setValue(String.valueOf(lockbouncer));
+        updateBouncerSummary(lockbouncer);
+	
+	  mLsSecurityAlpha = (SeekBarPreference) findPreference(LOCKSCREEN_SECURITY_ALPHA);
+        float alpha2 = Settings.System.getFloat(resolver,
+                Settings.System.LOCKSCREEN_SECURITY_ALPHA, 0.75f);
+        mLsSecurityAlpha.setValue((int)(100 * alpha2));
+        mLsSecurityAlpha.setOnPreferenceChangeListener(this);
     }
+
+    private void updateBouncerSummary(int value) {
+         Resources res = getResources();
+  
+         if (value == 0) {
+             // stock bouncer
+             mLsBouncer.setSummary(res.getString(R.string.ls_bouncer_on_summary));
+         } else if (value == 1) {
+             // bypass bouncer
+             mLsBouncer.setSummary(res.getString(R.string.ls_bouncer_off_summary));
+         } else {
+             String type = null;
+             switch (value) {
+                 case 2:
+                     type = res.getString(R.string.ls_bouncer_dismissable);
+                     break;
+                 case 3:
+                     type = res.getString(R.string.ls_bouncer_persistent);
+                     break;
+                 case 4:
+                     type = res.getString(R.string.ls_bouncer_all);
+                     break;
+             }
+             // Remove title capitalized formatting
+             type = type.toLowerCase();
+             mLsBouncer.setSummary(res.getString(R.string.ls_bouncer_summary, type));
+         }
+     }
 
     @Override
     public void onResume() {
@@ -71,6 +131,16 @@ public class LockScreenSettings extends SettingsPreferenceFragment implements On
                     Integer.valueOf((String) newValue));
             mLockClockFonts.setValue(String.valueOf(newValue));
             mLockClockFonts.setSummary(mLockClockFonts.getEntry());
+            return true;
+	}  else if (preference == mLsAlpha) {
+             int alpha = (Integer) newValue;
+             Settings.System.putFloat(resolver,
+                     Settings.System.LOCKSCREEN_ALPHA, alpha / 100.0f);
+             return true;
+        } else if (preference == mLsSecurityAlpha) {
+            int alpha2 = (Integer) newValue;
+            Settings.System.putFloat(resolver,
+                    Settings.System.LOCKSCREEN_SECURITY_ALPHA, alpha2 / 100.0f);
             return true;
         }
         return false;
