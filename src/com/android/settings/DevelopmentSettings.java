a3bfe4cab33dc06ab30ed95cb53ec2dc4a160b3f
Adrian DC <radian.dc@gmail.com>
2016-08-05 09:11:44 +0200

    Settings: Root appops access in developer settings
     * Allow users to directly access the root app settings,
        as the feature is mostly unknown and often a reason
        for users to try using alternatives to CM root
    
     * Use the appops extra intent to link the su tab,
        introduced in I79d963ecbadc624dc45a398387a348691318b6be
    
    Change-Id: Ia5bc9d56fb11700b19d73336fad13c0a936091ff
    Signed-off-by: Adrian DC <radian.dc@gmail.com>

--- src/com/android/settings/DevelopmentSettings.java
+++ src/com/android/settings/DevelopmentSettings.java
@@ -74,6 +74,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.android.internal.logging.MetricsLogger;
+import com.android.settings.Settings.AppOpsSummaryActivity;
 import com.android.settings.fuelgauge.InactiveApps;
 import com.android.settings.R;
 import com.android.settings.search.BaseSearchIndexProvider;
@@ -189,6 +190,8 @@ public class DevelopmentSettings extends SettingsPreferenceFragment
 
     private static final String ROOT_ACCESS_KEY = "root_access";
     private static final String ROOT_ACCESS_PROPERTY = "persist.sys.root_access";
+
+    private static final String ROOT_APPOPS_KEY = "root_appops";
 
     private static final String UPDATE_RECOVERY_KEY = "update_recovery";
     private static final String UPDATE_RECOVERY_PROPERTY = "persist.sys.recovery_update";
@@ -320,6 +323,8 @@ public class DevelopmentSettings extends SettingsPreferenceFragment
     private Object mSelectedRootValue;
     private PreferenceScreen mDevelopmentTools;
     private ColorModePreference mColorModePreference;
+
+    private Preference mRootAppops;
 
     private SwitchPreference mAdvancedReboot;
 
@@ -554,8 +559,13 @@ public class DevelopmentSettings extends SettingsPreferenceFragment
 
         mRootAccess = (ListPreference) findPreference(ROOT_ACCESS_KEY);
         mRootAccess.setOnPreferenceChangeListener(this);
+
+        mRootAppops = (Preference) findPreference(ROOT_APPOPS_KEY);
+        mRootAppops.setOnPreferenceClickListener(this);
+
         if (!removeRootOptionsIfRequired()) {
             mAllPrefs.add(mRootAccess);
+            mAllPrefs.add(mRootAppops);
         }
 
         mDevelopmentTools = (PreferenceScreen) findPreference(DEVELOPMENT_TOOLS);
@@ -930,6 +940,10 @@ public class DevelopmentSettings extends SettingsPreferenceFragment
         mRootAccess.setValue(value);
         mRootAccess.setSummary(getResources()
                 .getStringArray(R.array.root_access_entries)[Integer.valueOf(value)]);
+
+        if (mRootAppops != null) {
+            mRootAppops.setEnabled(isRootForAppsEnabled());
+        }
     }
 
     public static boolean isRootForAppsEnabled() {
@@ -2010,6 +2024,13 @@ public class DevelopmentSettings extends SettingsPreferenceFragment
                 preference == mTransitionAnimationScale ||
                 preference == mAnimatorDurationScale) {
             ((AnimationScalePreference) preference).click();
+        } else if (preference == mRootAppops) {
+            Activity mActivity = getActivity();
+            Intent intent = new Intent(Intent.ACTION_MAIN);
+            intent.putExtra("appops_tab", getString(R.string.app_ops_categories_su));
+            intent.setClass(mActivity, AppOpsSummaryActivity.class);
+            mActivity.startActivity(intent);
+            return true;
         }
         return false;
     }
