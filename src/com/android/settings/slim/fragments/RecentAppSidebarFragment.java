3ae43bd30ba6a4a49596afefde9e581bd58f0b58
Varun Date <date.varun123@gmail.com>
2016-08-26 17:33:12 -0700

    Update Slim Recent App Sidebar (2/2)
    - Add setting to disable sidebar without removing items
    - Add setting to not open simultaneously with recents in order to reduce recents lag when opening
    
    Change-Id: I9421cc97bfabeeeb3b656fa4281766841213951a
    Signed-off-by: Varun Date <date.varun123@gmail.com>

--- src/com/android/settings/slim/fragments/RecentAppSidebarFragment.java
+++ src/com/android/settings/slim/fragments/RecentAppSidebarFragment.java
@@ -17,6 +17,9 @@
 package com.android.settings.slim.fragments;
 
 import android.os.Bundle;
+import android.preference.Preference;
+import android.preference.SwitchPreference;
+import android.provider.Settings;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -27,7 +30,13 @@ import com.android.settings.SettingsPreferenceFragment;
 
 import org.cyanogenmod.internal.logging.CMMetricsLogger;
 
-public class RecentAppSidebarFragment extends SettingsPreferenceFragment {
+public class RecentAppSidebarFragment extends SettingsPreferenceFragment implements
+        Preference.OnPreferenceChangeListener {
+
+    // Preferences
+    private static final String USE_RECENT_APP_SIDEBAR = "use_recent_app_sidebar";
+
+    private SwitchPreference mUseSidebar;
 
     @Override
     protected int getMetricsCategory() {
@@ -39,6 +48,7 @@ public class RecentAppSidebarFragment extends SettingsPreferenceFragment {
         super.onCreate(savedInstanceState);
 
         addPreferencesFromResource(R.xml.recent_app_sidebar_fragment);
+        initializeAllPreferences();
     }
 
     @Override
@@ -53,5 +63,22 @@ public class RecentAppSidebarFragment extends SettingsPreferenceFragment {
             list.setPadding(0, paddingTop, 0, paddingBottom);
         }
         return view;
+    }
+
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == mUseSidebar) {
+            Settings.System.putInt(getContentResolver(),
+                    Settings.System.USE_RECENT_APP_SIDEBAR,
+                    ((Boolean) newValue) ? 1 : 0);
+            return true;
+        }
+        return false;
+    }
+
+    private void initializeAllPreferences() {
+        mUseSidebar = (SwitchPreference) findPreference(USE_RECENT_APP_SIDEBAR);
+        mUseSidebar.setOnPreferenceChangeListener(this);
+        mUseSidebar.setChecked(Settings.System.getInt(getContentResolver(),
+                Settings.System.USE_RECENT_APP_SIDEBAR, 1) == 1);
     }
 }
